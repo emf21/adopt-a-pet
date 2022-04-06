6d6c127d26c72e4a3aa909ab18934592fbe63494
emf21 <emefa.ameku@amalitech.org>
2022-04-06 04:06:33 +0000

    complete version of project adopt a pet

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      {/* to create an optional parameter, you need to append ? */}
+      <Switch>
+        <Route path="/home-page/:type?">
+          <HomePage />
+        </Route>
+
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+
+        <Route path="/pet-details-not-found">
+          <PetDetailsNotFound />
+        </Route>
+
+        <Route path="/:type/:id">
+          <PetDetailsPage />
+        </Route>
+
+        <Route path="/:type?">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
